fdf0daeffa3e4a23385d4dc9640961440a063cc4
Dmtry Golz <dmsg2017@outlook.com>
2025-08-22 14:53:04 +0300

    Heavy Resourse & Lazy initialisation added

--- src/main/java/com/golzstore/springstore/AppConfig.java
+++ src/main/java/com/golzstore/springstore/AppConfig.java
@@ -3,6 +3,7 @@ package com.golzstore.springstore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 
 @Configuration

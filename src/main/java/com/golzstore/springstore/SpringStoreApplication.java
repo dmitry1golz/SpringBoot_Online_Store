db22a0d616fbd7a1f88ea38189aa2aef912182bf
Dmtry Golz <dmsg2017@outlook.com>
2025-08-23 22:06:54 +0300

    DB 0.01v

--- src/main/java/com/golzstore/springstore/SpringStoreApplication.java
+++ src/main/java/com/golzstore/springstore/SpringStoreApplication.java
@@ -9,8 +9,7 @@ public class SpringStoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringStoreApplication.class, args);
-        var userService = context.getBean(UserService.class);
-        userService.registerUser(new User(1L, "dmitryGolz@gmail.com", "Dmitry Golz", "12345678" ));
+
 
     }
 

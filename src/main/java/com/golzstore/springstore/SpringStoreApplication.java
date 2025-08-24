2380bdd993f2b36e82e48318d1828ae12831af82
Dmtry Golz <dmsg2017@outlook.com>
2025-08-24 23:59:49 +0300

    relations in App added

--- src/main/java/com/golzstore/springstore/SpringStoreApplication.java
+++ src/main/java/com/golzstore/springstore/SpringStoreApplication.java
@@ -1,15 +1,24 @@
 package com.golzstore.springstore;
 
-import org.springframework.boot.SpringApplication;
+import com.golzstore.springstore.entities.Address;
+import com.golzstore.springstore.entities.Profile;
+import com.golzstore.springstore.entities.User;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringStoreApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(SpringStoreApplication.class, args);
+//        ApplicationContext context = SpringApplication.run(SpringStoreApplication.class, args);
+        var user = User.builder().name("Dmitry").email("email").password("password").build();
 
+        var address = Address.builder().street("123 Main St").city("Main St").state("Main State").zip("12345").build();
+
+        var profile = Profile.builder().bio("bio").build();
+        user.setProfile(profile);
+        profile.setUser(user);
+
+        System.out.println(user);
 
     }
 

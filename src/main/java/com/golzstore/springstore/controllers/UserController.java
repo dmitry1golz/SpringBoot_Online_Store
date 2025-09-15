b0833498c647c0361d6b150bceb4b7d3de935cb2
Dmtry Golz <dmsg2017@outlook.com>
2025-09-15 16:54:44 +0300

    Exeption handler for USER controller added added

--- src/main/java/com/golzstore/springstore/controllers/UserController.java
+++ src/main/java/com/golzstore/springstore/controllers/UserController.java
@@ -11,9 +11,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -43,9 +46,8 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(
-            @Valid @RequestBody RegisterUserRequest request,
-            UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody RegisterUserRequest request,
+                                              UriComponentsBuilder uriBuilder) {
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 
@@ -97,6 +99,15 @@ public class UserController {
 
         return ResponseEntity.noContent().build();
 
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException exception) {
+        var errors = new HashMap<String, String>();
+
+        exception.getBindingResult().getFieldErrors()
+                 .forEach((error) -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
     }
 
 }

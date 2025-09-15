b26b2299569a9aef863ee2beee38572a8363b864
Dmtry Golz <dmsg2017@outlook.com>
2025-09-15 19:31:24 +0300

    email if existed: business validator added

--- src/main/java/com/golzstore/springstore/repositories/UserRepository.java
+++ src/main/java/com/golzstore/springstore/repositories/UserRepository.java
@@ -4,5 +4,6 @@ import com.golzstore.springstore.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository  extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }

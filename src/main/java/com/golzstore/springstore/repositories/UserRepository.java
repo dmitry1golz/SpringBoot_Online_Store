8c382caa7c8551c0748ef76238aecba291d21e0e
Dmtry Golz <dmsg2017@outlook.com>
2025-09-24 18:32:47 +0300

    security Service Manager added

--- src/main/java/com/golzstore/springstore/repositories/UserRepository.java
+++ src/main/java/com/golzstore/springstore/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.golzstore.springstore.repositories;
-
 import com.golzstore.springstore.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }

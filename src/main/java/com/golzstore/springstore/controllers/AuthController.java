b97dcd1b71323d38b0142df953b1277e3ba99918
Dmtry Golz <dmsg2017@outlook.com>
2025-09-24 19:36:47 +0300

    secret token added

--- src/main/java/com/golzstore/springstore/controllers/AuthController.java
+++ src/main/java/com/golzstore/springstore/controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.golzstore.springstore.controllers;
 
+import com.golzstore.springstore.dtos.JwtResponse;
 import com.golzstore.springstore.dtos.LoginRequest;
+import com.golzstore.springstore.service.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,16 +17,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthenticationManager authenticationManager;
-
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                 request.getEmail(),
                 request.getPassword()));
 
-        return ResponseEntity.ok().build();
+        var token = jwtService.generateToken(request.getEmail());
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
     @ExceptionHandler({BadCredentialsException.class})
---
+++ src/main/java/com/golzstore/springstore/dtos/JwtResponse.java
@@ -0,0 +1,11 @@
+package com.golzstore.springstore.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class JwtResponse {
+    private String token;
+
+}

961215f4c50f2a9ad9295d2dc96a2203f9f27ada
Dmtry Golz <dmsg2017@outlook.com>
2025-09-25 18:15:25 +0300

    f

--- src/main/java/com/golzstore/springstore/controllers/AuthController.java
+++ src/main/java/com/golzstore/springstore/controllers/AuthController.java
@@ -2,12 +2,18 @@ package com.golzstore.springstore.controllers;
 
 import com.golzstore.springstore.dtos.JwtResponse;
 import com.golzstore.springstore.dtos.LoginRequest;
+import com.golzstore.springstore.dtos.UserDto;
+import com.golzstore.springstore.mappers.UserMapper;
+import com.golzstore.springstore.repositories.UserRepository;
 import com.golzstore.springstore.service.JwtService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
@@ -16,6 +22,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid @RequestBody LoginRequest request) {
@@ -36,5 +44,24 @@ public class AuthController {
         return jwtService.validateToken(token);
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var email = (String) authentication.getPrincipal();
+
+        var user = userRepository.findByEmail(email).orElse(null);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = userMapper.toDto(user);
+
+        return ResponseEntity.ok(userDto);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Void> handleBadCredentialsException() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
 
 }

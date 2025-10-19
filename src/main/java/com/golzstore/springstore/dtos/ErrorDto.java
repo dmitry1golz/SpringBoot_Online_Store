333cd6ead4d49e352dd18c3cfb193fb6d9688b24
Dmtry Golz <dmsg2017@outlook.com>
2025-10-19 15:44:50 +0300

    ErrorDto added

---
+++ src/main/java/com/golzstore/springstore/dtos/ErrorDto.java
@@ -0,0 +1,12 @@
+package com.golzstore.springstore.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorDto {
+    private String error;
+
+
+}

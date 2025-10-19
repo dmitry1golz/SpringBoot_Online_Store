7f853931790bf3bb03f76ae97c31e56339309f2c
Dmtry Golz <dmsg2017@outlook.com>
2025-10-19 15:44:30 +0300

    Auth Service added

---
+++ src/main/java/com/golzstore/springstore/service/AuthService.java
@@ -0,0 +1,20 @@
+package com.golzstore.springstore.service;
+
+import com.golzstore.springstore.entities.User;
+import com.golzstore.springstore.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+@AllArgsConstructor
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    public User getCurrentUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var userId = (Long) authentication.getPrincipal();
+
+        return userRepository.findById(userId).orElse(null);
+    }
+
+}

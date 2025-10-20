bf01ac77ef18bf704ea14a16db192f38018b3b03
Dmtry Golz <dmsg2017@outlook.com>
2025-10-20 15:43:51 +0300

    add order Service

--- src/main/java/com/golzstore/springstore/service/OrderService.java
+++ src/main/java/com/golzstore/springstore/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.golzstore.springstore.service;
 
 import com.golzstore.springstore.dtos.OrderDto;
+import com.golzstore.springstore.exceptions.OrderNotFoundException;
 import com.golzstore.springstore.mappers.OrderMapper;
 import com.golzstore.springstore.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,7 +19,18 @@ public class OrderService {
 
     public List<OrderDto> getAllOrders() {
         var user = authService.getCurrentUser();
-        var orders = orderRepository.getAllByCustomer(user);
+        var orders = orderRepository.getOrdersByCustomer(user);
         return orders.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrder(Long orderId) {
+        var order = orderRepository.getOrderWithItems(orderId).orElseThrow(OrderNotFoundException::new);
+
+        var user = authService.getCurrentUser();
+        if (!order.isPlacedBy(user)) {
+            throw new AccessDeniedException("You dont have access to this order.");
+        }
+
+        return orderMapper.toDto(order);
     }
 }

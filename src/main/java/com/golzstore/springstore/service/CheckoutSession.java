a1bd4e601abc7d98bf08c269c923133cca31a083
Dmtry Golz <dmsg2017@outlook.com>
2025-10-24 16:01:58 +0300

    CheckoutSession added

---
+++ src/main/java/com/golzstore/springstore/service/CheckoutSession.java
@@ -0,0 +1,10 @@
+package com.golzstore.springstore.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CheckoutSession {
+    private String checkoutUrl;
+}

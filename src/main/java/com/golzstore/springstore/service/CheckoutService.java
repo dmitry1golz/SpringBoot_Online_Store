f045fa0dae50ab83b7f4490c2c083adbc31b07a5
Dmtry Golz <dmsg2017@outlook.com>
2025-10-24 16:02:37 +0300

    CheckoutService Transactional added

--- src/main/java/com/golzstore/springstore/service/CheckoutService.java
+++ src/main/java/com/golzstore/springstore/service/CheckoutService.java
@@ -5,19 +5,25 @@ import com.golzstore.springstore.dtos.CheckoutResponse;
 import com.golzstore.springstore.entities.Order;
 import com.golzstore.springstore.exceptions.CartEmptyException;
 import com.golzstore.springstore.exceptions.CartNotFoundException;
+import com.golzstore.springstore.exceptions.PaymentException;
 import com.golzstore.springstore.repositories.CartRepository;
 import com.golzstore.springstore.repositories.OrderRepository;
-import lombok.AllArgsConstructor;
+import com.stripe.exception.StripeException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class CheckoutService {
     private final CartRepository cartRepository;
     private final OrderRepository orderRepository;
     private final AuthService authService;
     private final CartService cartService;
+    private final PaymentGateway paymentGateway;
 
+    @Transactional
     public CheckoutResponse checkout(CheckoutRequest request) {
         var cart = cartRepository.getCartWithItems(request.getCartId()).orElse(null);
         if (cart == null) {
@@ -30,9 +36,16 @@ public class CheckoutService {
         var order = Order.fromCart(cart, authService.getCurrentUser());
 
         orderRepository.save(order);
+        try {
 
-        cartService.clearCart(cart.getId());
-        return new CheckoutResponse(order.getId());
+            var session = paymentGateway.createCheckoutSession(order);
+
+            cartService.clearCart(cart.getId());
+            return new CheckoutResponse(order.getId(), session.getCheckoutUrl());
+        } catch (PaymentException ex) {
+            orderRepository.delete(order);
+            throw ex;
+        }
     }
 
 }

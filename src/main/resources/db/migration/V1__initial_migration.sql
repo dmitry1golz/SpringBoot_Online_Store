62bfe3724532a83464c9e6f77365b55e472bfada
Dmitry_Golz <Dmsg2017@outlook.com>
2026-08-07 00:53:14 +0200

    style(db): align column definitions in initial migration

--- src/main/resources/db/migration/V1__initial_migration.sql
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -1,37 +1,37 @@
 CREATE TABLE addresses
 (
     id      BIGINT AUTO_INCREMENT NOT NULL,
-    street  VARCHAR(255) NOT NULL,
-    city    VARCHAR(255) NOT NULL,
-    state   VARCHAR(255) NOT NULL,
-    zip     VARCHAR(255) NOT NULL,
-    user_id BIGINT       NOT NULL,
+    street  VARCHAR(255)          NOT NULL,
+    city    VARCHAR(255)          NOT NULL,
+    state   VARCHAR(255)          NOT NULL,
+    zip     VARCHAR(255)          NOT NULL,
+    user_id BIGINT                NOT NULL,
     CONSTRAINT `PRIMARY` PRIMARY KEY (id)
 );
 
 CREATE TABLE categories
 (
     id   TINYINT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255)           NOT NULL,
     CONSTRAINT `PRIMARY` PRIMARY KEY (id)
 );
 
 CREATE TABLE products
 (
     id            BIGINT AUTO_INCREMENT NOT NULL,
-    name          VARCHAR(255)   NOT NULL,
-    price         DECIMAL(10, 2) NOT NULL,
-    `description` LONGTEXT       NOT NULL,
-    category_id   TINYINT NULL,
+    name          VARCHAR(255)          NOT NULL,
+    price         DECIMAL(10, 2)        NOT NULL,
+    `description` LONGTEXT              NOT NULL,
+    category_id   TINYINT               NULL,
     CONSTRAINT `PRIMARY` PRIMARY KEY (id)
 );
 
 CREATE TABLE profiles
 (
-    id             BIGINT NOT NULL,
-    bio            LONGTEXT NULL,
-    phone_number   VARCHAR(15) NULL,
-    date_of_birth  date NULL,
+    id             BIGINT                 NOT NULL,
+    bio            LONGTEXT               NULL,
+    phone_number   VARCHAR(15)            NULL,
+    date_of_birth  date                   NULL,
     loyalty_points INT UNSIGNED DEFAULT 0 NULL,
     CONSTRAINT `PRIMARY` PRIMARY KEY (id)
 );
@@ -39,9 +39,9 @@ CREATE TABLE profiles
 CREATE TABLE users
 (
     id       BIGINT AUTO_INCREMENT NOT NULL,
-    name     VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    name     VARCHAR(255)          NOT NULL,
+    email    VARCHAR(255)          NOT NULL,
+    password VARCHAR(255)          NOT NULL,
     CONSTRAINT `PRIMARY` PRIMARY KEY (id)
 );
 
